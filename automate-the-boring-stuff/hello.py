c75c120905cec8d5bc62c895e312122410424102
hdolinh <halina@bren.ucsb.edu>
2023-09-02 12:45:14 -0700

    add string formatting ex's

--- automate-the-boring-stuff/hello.py
+++ automate-the-boring-stuff/hello.py
@@ -7,3 +7,8 @@ print("I can tell you the length of your name is: " + str(int(len(myName))))
 print("What is your age?")
 myAge = input()
 print("You will be " + str(int(myAge) + 1) + " in a year.")
+
+# formatting strings
+# three different ways
+print('I am ' + str(29) + ' years old.')
+print("I am {} years old.".format(29))
