526237ac6b553ece0cbe875d79049efa2da092ad
hdolinh <halina@bren.ucsb.edu>
2023-09-02 01:29:58 -0700

    create script for py basics

---
+++ my_script.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+# assigning values to vars
+text = "Data Carpentry"
+number = 42
+pi_value = 3.1415
+
+# use print() to display an outpu in a script
+print(text)
+print(number)
+
+# operators
+2 ** 16 # power
+13 % 5 # modulo
+
+# lists
+numbers = [1, 2, 3]
+numbers[0] # indexing starts with 0
