e8bd25a1e93a87ea72147d520683dd561ac08dd2
hdolinh <halina@bren.ucsb.edu>
2023-09-02 08:33:47 -0700

    ex5 LPTHW

---
+++ learn-python-the-hard-way/ex5.py
@@ -0,0 +1,35 @@
+# more variables and printing #
+
+# embed variables inside a string using `{}`
+# must start the string with the letter 'f'
+# 'f' means 'format'
+# ex: f"Hellow {somevar}" tells python this string needs to be formatted, put the vars in there
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+# convert inches to centimeters
+cm = 2.54
+print(f"{name}'s height in centimeters is {height * cm}.")
+# round centimeters
+print(f"{name}'s height in centimeters is {round(height * cm)}.")
+
+# convert pounds to kilograms
+kg = 2.205
+print(f"{name}'s weight in kilograms is {round(weight / kg)}.")
